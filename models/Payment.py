2056efd1b2af9bc4f7d00a0958504605a5b56573
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-10-27 11:28:21 +1300

    Update Payment.py

--- models/Payment.py
+++ models/Payment.py
@@ -1,40 +1,19 @@
 from sqlalchemy import Column, Integer, String, Float, Date
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from .base import Base
 
 class Payment(Base):
     __tablename__ = 'payments'
 
-    __paymentID = Column(Integer, primary_key=True, autoincrement=True)
-    __paymentAmount = Column(Float)
-    __paymentDate = Column(Date)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    paymentAmount = Column(Float)
+    paymentDate = Column(Date)
     type = Column(String(50))
 
     __mapper_args__ = {
-        'polymorphic_identity': 'Payment', # Use 'type' column to distinguish subclasses
-        'polymorphic_on': type
+        'polymorphic_identity': 'Payment',  # The base identity for this class
+        'polymorphic_on': type              # SQLAlchemy uses 'type' to distinguish subclasses
     }
 
     def __init__(self, paymentAmount, paymentDate):
-        self.__paymentAmount = paymentAmount
-        self.__paymentDate = paymentDate
-
-    # Getter and setter for paymentAmount
-    @property
-    def paymentAmount(self):
-        return self.__paymentAmount
-
-    @paymentAmount.setter
-    def paymentAmount(self, value):
-        # limit the payment amount to 2 decimals
-        self.__paymentAmount = round(value, 2)
-
-    # Getter and setter for paymentDate
-    @property
-    def paymentDate(self):
-        return self.__paymentDate
-
-    @paymentDate.setter
-    def paymentDate(self, value):
-        self.__paymentDate = value
+        self.paymentAmount = paymentAmount
+        self.paymentDate = paymentDate
