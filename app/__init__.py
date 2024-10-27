b86295ba6078ea01c9744356574dfd3b3347169c
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-10-27 15:51:03 +1300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Crazy2447YT!@localhost:3306/FreshHarvestVeggies'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
